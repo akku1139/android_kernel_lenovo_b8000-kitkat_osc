4a7f28f0efbbcb44ec884fcd81aded26bc137aa5
akku <akku@akku.dedyn.io>
2024-09-16 00:08:58 +0900

    Debug env

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ export CROSS_COMPILE="`dirname $BASH_SOURCE`/toolchain/arm-cortex_a7-linux-gnuea
 export KCFLAGS="-pipe -mtune=cortex-a7 -march=armv7-a -mfpu=neon-vfpv4"
 export MTK_ROOT_CUSTOM="`dirname $BASH_SOURCE`/mediatek/custom/"
 
+echo $CROSS_COMPILE $MTK_ROOT_CUSTOM
+
 cd kernel
 bash build.sh lenovo89_tb_x10_jb2
