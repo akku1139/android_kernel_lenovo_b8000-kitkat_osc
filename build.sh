0d309173a8467761234e705c6e3fb40098a5de0d
akku <akku@akku.dedyn.io>
2024-09-16 00:42:57 +0900

    Fix path

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-cd `dirname $BASH_SOURCE`
+cd `dirname $BASH_SOURCE`/kernel
 
 export CROSS_COMPILE="`dirname $BASH_SOURCE`/toolchain/arm-cortex_a7-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a7-linux-gnueabihf-"
 export KCFLAGS="-pipe -mtune=cortex-a7 -march=armv7-a -mfpu=neon-vfpv4"
 export MTK_ROOT_CUSTOM="`dirname $BASH_SOURCE`/mediatek/custom/"
 
-echo $CROSS_COMPILE $MTK_ROOT_CUSTOM
+echo $CROSS_COMPILE
+echo $MTK_ROOT_CUSTOM
 
-cd kernel
 bash build.sh lenovo89_tb_x10_jb2
