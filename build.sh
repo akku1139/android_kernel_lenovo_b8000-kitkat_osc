3b1fda554eab05ba6c060cc0fbdeb8d972978985
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-05 01:15:32 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 cd `dirname $BASH_SOURCE`
 
 IT=$(cd $(dirname $BASH_SOURCE); pwd)
+export ARCH=arm
 export CROSS_COMPILE="ccache $IT/toolchain/arm-cortex_a7-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a7-linux-gnueabihf-"
 export KCFLAGS="-pipe -mtune=cortex-a7 -march=armv7-a -mfpu=neon-vfpv4"
 export MTK_ROOT_CUSTOM="$IT/mediatek/custom/"
